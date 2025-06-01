e16086d8ceb38f952f191cc76c9df896dd453b8e
EhsnAbdllh <sha.m.a.r.8315@gamil.com>
2025-06-01 19:20:03 +0330

    feat: (New data model)

---
+++ Golestan_Simulation/Models/Classroom.cs
@@ -0,0 +1,10 @@
+﻿namespace Golestan_Simulation.Models
+{
+    public class Classroom
+    {
+        public int Class_Id { get; set; }
+        public string? Class_Building { get; set; }
+        public int Class_RoomNumber { get; set; }
+        public int Class_Capacity { get; set; }
+    }
+}
