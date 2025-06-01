7efc1d0fa582e2536ceb12184f3d5e44ad7d8671
EhsnAbdllh <sha.m.a.r.8315@gamil.com>
2025-06-01 19:17:19 +0330

    feat: (New data model)

---
+++ Golestan_Simulation/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace Golestan_Simulation.Models
+{
+    public class Courses
+    {
+        public int Course_Id { get; set;}
+        public string? Course_Name { get; set; }
+        public string? Course_Code { get; set; }
+        public string? Course_Unit { get; set; }
+        public string? Course_Description { get; set; }
+        public DateTime Course_Exame_Date { get; set; }
+    }
+}
