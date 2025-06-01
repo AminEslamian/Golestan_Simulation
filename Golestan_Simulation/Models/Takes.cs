2ff5d4db17a77d171ea3034d1ba41295df951cd9
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-06-01 19:40:29 +0330

    feat: add Takes module

---
+++ Golestan_Simulation/Models/Takes.cs
@@ -0,0 +1,9 @@
+﻿namespace Golestan_Simulation.Models
+{
+    public class Takes
+    {
+        public int Student_Id { get; set; }
+        public int Section_Id { get; set; }
+        public int Grade { get; set; }
+    }
+}
