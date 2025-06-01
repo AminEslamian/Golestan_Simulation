4dad459f24f614c4206815c10d11751db4fbd722
EhsnAbdllh <sha.m.a.r.8315@gamil.com>
2025-06-01 19:23:26 +0330

    feat: (New data model)

---
+++ Golestan_Simulation/Models/TimeSlots.cs
@@ -0,0 +1,10 @@
+﻿namespace Golestan_Simulation.Models
+{
+    public class TimeSlots
+    {
+        public int TimeSlot_Id { get; set; }
+        public string? TimeSlot_Day { get; set; }
+        public DateTime TimeSlot_StartTime { get; set; }
+        public DateTime TimeSlot_EndTime { get; set; }
+    }
+}
