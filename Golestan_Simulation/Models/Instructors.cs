53011661ea8f9ba1e0380db533544c1a9d64a113
EhsnAbdllh <sha.m.a.r.8315@gamil.com>
2025-06-01 19:11:12 +0330

    feat: (New data model)

---
+++ Golestan_Simulation/Models/Instructors.cs
@@ -0,0 +1,10 @@
+﻿namespace Golestan_Simulation.Models
+{
+    public class Instructors
+    {
+        public int Instructor_Id { get; set; }
+        public int User_Id { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Hire_Date { get; set; }
+    }
+}
