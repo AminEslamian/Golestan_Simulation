5256d17cfe89e63ddad700ce44a55e4304519103
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-06-09 13:11:42 +0330

    feat: add DbSets in ApplicationDbContext

--- Golestan_Simulation/Data/ApplicationDbContext.cs
+++ Golestan_Simulation/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Golestan_Simulation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Golestan_Simulation.Data
 {
@@ -8,6 +9,18 @@ namespace Golestan_Simulation.Data
             : base(options)
         {
         }
+
+        public DbSet<Classrooms> Classrooms { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<Instructors> Instructors { get; set; }
+        // public DbSet<Roles> Roles { get; set; } // Probably not needed, but wasn't sure!
+        public DbSet<Sections> Sections { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Takes> Takes { get; set; }
+        public DbSet<Teachs> Teaches { get; set; }
+        public DbSet<TimeSlots> TimeSlots { get; set; }
+        public DbSet<UserRoles> UserRoles { get; set; }
+        public DbSet<Users> Users { get; set; }
 
     }
 }
