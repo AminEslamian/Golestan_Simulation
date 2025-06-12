91b55e0eeb1e2d0825a2ccb3dbb0955f29b27c09
Amin <90086058+AminEslamian@users.noreply.github.com>
2025-06-12 22:44:47 +0330

    feat (model): Add dbContext, migration & basic controllers
    * feat: add the DbContext class
    
    * chore: install packages
    
    * feat: add DbContext to [Program.cs]
    
    * docs: add connection configuration guidance
    
    * fix: made the StudentId as primarlyKey
    
    * fix: uncommented a disabled DbSet in DbContext
    
    * feat: configure Takes model to 'Keyless' via data annotation
    
    * feat: configure Teachs & UserRoles models to 'Keyless'
    
    * feat: configure connection settings in [appsettings.json]
    
    * fix: fix connection configuration in [Program.cs]
    
    * docs: remove unneccesary comments
    
    * feat: add 3 role-based controllers
    
    * feat: Scaffole CheetsheetController based on User model

---
+++ Golestan_Simulation/Controllers/CheetsheetController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Golestan_Simulation.Data;
+using Golestan_Simulation.Models;
+
+namespace Golestan_Simulation.Controllers
+{
+    public class CheetsheetController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CheetsheetController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cheetsheet
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Cheetsheet/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+        // GET: Cheetsheet/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cheetsheet/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreatedAt,FirstName,LastName,Email,HashedPassword")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(users);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(users);
+        }
+
+        // GET: Cheetsheet/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Cheetsheet/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CreatedAt,FirstName,LastName,Email,HashedPassword")] Users users)
+        {
+            if (id != users.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(users);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsersExists(users.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(users);
+        }
+
+        // GET: Cheetsheet/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+        // POST: Cheetsheet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            if (users != null)
+            {
+                _context.Users.Remove(users);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsersExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
--- Golestan_Simulation/Program.cs
+++ Golestan_Simulation/Program.cs
@@ -1,7 +1,14 @@
+using Golestan_Simulation.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"))); // ### configure the DefaultConnection ConnectionString in the appsettings.json file
 
 var app = builder.Build();
 
