15c25d40ade6feda9d9a178539f93074f8372d7e
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-06-16 17:12:35 +0330

    feat: add Configuration file for database configurations

---
+++ Golestan_Simulation/Data/Configurations.cs
@@ -0,0 +1,25 @@
+﻿using Golestan_Simulation.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Golestan_Simulation.Data
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Students>
+    {
+        public void Configure(EntityTypeBuilder<Students> builder)
+        {
+            // ────────────────
+            // 1) Entity‑level Configuration
+            // ────────────────
+
+
+
+
+            // ────────────────
+            // 2) Property‑level Configuration
+            // ────────────────
+        }
+    }
+
+}
