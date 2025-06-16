10316b5e93db534e7b2e7e5298c5a43de892b329
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-06-16 17:36:36 +0330

    feat: configure Student fluent api

--- Golestan_Simulation/Data/Configurations.cs
+++ Golestan_Simulation/Data/Configurations.cs
@@ -13,12 +13,19 @@ namespace Golestan_Simulation.Data
             // 1) Entity‑level Configuration
             // ────────────────
 
+            builder.ToTable("Students");
 
+            builder.HasKey(e => e.StudentId);
 
+            builder.HasOne(s => s.User) // Student has one User
+                .WithMany(u => u.Students) // User has many Students
+                .HasForeignKey(s => s.UserId); // FK is on Student.UserId
 
             // ────────────────
             // 2) Property‑level Configuration
             // ────────────────
+
+
         }
     }
 
