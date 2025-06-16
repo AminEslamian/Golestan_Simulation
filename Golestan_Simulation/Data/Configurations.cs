759c37b2d5c3d55f0f3d02cc24f8b7783aebb78d
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-06-16 19:19:32 +0330

    feat: add UserConfiguration

--- Golestan_Simulation/Data/Configurations.cs
+++ Golestan_Simulation/Data/Configurations.cs
@@ -25,10 +25,56 @@ namespace Golestan_Simulation.Data
             // 2) Property‑level Configuration
             // ────────────────
 
-            builder.Property(e => e.EnrollmentDate)
+            builder.Property(e => e.EnrollmentDate) // ## This approch might need to be reconsidered! ##
                 .HasColumnName("EnrollmentDate")
                 .HasColumnType("date")
                 .HasDefaultValueSql("getdate()");
+        }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builer)
+        {
+            // ────────────────
+            // 1) Entity‑level Configuration
+            // ────────────────
+
+            builer.ToTable("Users");
+            builer.HasKey(e => e.Id);
+
+            // ─────────── Students relationship ───────────
+
+            builer.HasMany(u => u.Students)
+                .WithOne(s => s.User)
+                .HasForeignKey(s => s.UserId);
+
+            // ─────────── Instructor relationship ───────────
+            builer.HasMany(u => u.Instructors)
+                .WithOne(i => i.User)
+                .HasForeignKey(i => i.Id);
+
+            // ─────────── User roles relationship ───────────
+            // ### is it needed to add or not?
+
+
+            // ────────────────
+            // 2) Property‑level Configuration
+            // ────────────────
+
+            builer.Property(e => e.FirstName)
+                .HasColumnName("FirstName");
+            builer.Property(e => e.LastName)
+                .HasColumnName("LastName");
+            builer.Property(e => e.Email)
+                .HasColumnName("Email");
+            builer.Property(e => e.HashedPassword)
+                .HasColumnName("HashedPassword");
+            builer.Property(e => e.CreatedAt)
+                .HasColumnName("CreatedAt")
+                .HasColumnType("date")
+                .HasDefaultValueSql("getdate()");
+
         }
     }
 
