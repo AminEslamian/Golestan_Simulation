7654375501ee5f2010f9cd72ea6889d15ca3fde6
EhsnAbdllh <sha.m.a.r.8315@gmail.com>
2025-07-10 10:14:59 +0330

    feat: add services, actions and views for cookie based authentication and login (#17)
    * feat: add view model and required services for login action
    
    - IsAccountExisting: to find corresponding UserRoles record
    _ IsPasswordCorrect: to check input password correctness
    - UserViewModle: a view model to be used on view for login action
    
    * feat: add http get & post actions for login
    
    - AccessDenied: pass to access denied page after authorization
    
    * feat: add razor page and required changes on view layer for login action
    
    - Index: add 2 button which specify the role of user who is logging in
    
    * feat: inject  new services for authentication and cookies
    
    * refactor: add new migration
    
    * chore: drop database and remove all migrations
    
    * chore: add a new migration and update database
    
    * fix: add exception handle for querying to database
    
    _ problem: while someone login before any user register, a ArgumentNullException thrown when the program send the query to database
    
    * feat: add redirect to dashboard page
    
    * Revert "fix: add exception handle for querying to database"
    
    This reverts commit a5bdffa615aa49f69e4f6564cf5910e3f7150270.
    
    * feat: change functionality of http get method of login action
    
    - old one: specify the role based on a hiddent filed (namely "Role") in view.
    But when the authentication service pass the client to "loginPath", the field remains null.
    - new one: specify the role based on the root of "returnUrl" that acts for both events.
    
    * feat: add authorization for controllers
    
    * fix: handle "InvalidOperationException" in "UserAccountServices"
    
    problem: when no instance of accounts have been initialized, the query to database fails, because the InvalidOperationExcaption throws.
    
    * refactor: rename some files, and change or add some HTML things
    
    ---------
    
    Co-authored-by: EhsnAbdllh <sha.m.a.r.8315@gamil.com>
    Co-authored-by: Amin <90086058+AminEslamian@users.noreply.github.com>

--- Golestan_Simulation/Controllers/HomeController.cs
+++ Golestan_Simulation/Controllers/HomeController.cs
@@ -1,31 +1,109 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Golestan_Simulation.Models;
+using Golestan_Simulation.Services;
+using Golestan_Simulation.ViewModels;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Golestan_Simulation.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUserAccountServices _accountServices;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUserAccountServices accountServices)
     {
         _logger = logger;
+        _accountServices = accountServices;
     }
+
 
     public IActionResult Index()
     {
         return View();
     }
 
+
     public IActionResult Privacy()
     {
         return View();
     }
 
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+    public IActionResult Login(string? returnUrl = null)
+    {
+        RolesEnum expectedRole = RolesEnum.None;
+        if (!string.IsNullOrEmpty(returnUrl))
+        {
+            if (returnUrl.StartsWith("/admin", StringComparison.OrdinalIgnoreCase))
+                expectedRole = RolesEnum.Admin;
+            else if (returnUrl.StartsWith("/instructor", StringComparison.OrdinalIgnoreCase))
+                expectedRole = RolesEnum.Instructor;
+            else if (returnUrl.StartsWith("/student", StringComparison.OrdinalIgnoreCase))
+                expectedRole = RolesEnum.Student;
+        }
+        else
+            return NotFound();
+
+            ViewBag.Role = expectedRole;
+
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(UserViewModel user, RolesEnum role)
+    {
+        if (ModelState.IsValid)
+        {
+            var userRole = await _accountServices.IsAccountExistingAsync(user, role);
+            if (userRole == null)
+            {
+                ModelState.AddModelError("UsernameOrEmail", "No account found with this user name or email");
+                return View(user);
+            }
+
+            if (_accountServices.IsPasswordCorrect(userRole, user.RawPassword))
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userRole.User.UserName),
+                    new Claim(ClaimTypes.Role, userRole.Role.Name.ToString()),
+                    new Claim("UserId", userRole.UserId.ToString())
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                if (role == RolesEnum.Instructor)
+                    return RedirectToAction("Index", "Instructor");
+                if (role == RolesEnum.Student)
+                    return RedirectToAction("Index", "Student");
+            }
+            else
+            {
+                ModelState.AddModelError("RawPassword", "The password is not correct");
+                return View(user);
+            }
+
+        }
+        return View(user);
+    }
+
+
+    public IActionResult AccessDenied()
+    {
+        return View();
     }
 }
--- Golestan_Simulation/Services/UserAccountServices.cs
+++ Golestan_Simulation/Services/UserAccountServices.cs
@@ -1,4 +1,6 @@
 ﻿using Golestan_Simulation.Data;
+using Golestan_Simulation.Models;
+using Golestan_Simulation.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,16 +10,21 @@ namespace Golestan_Simulation.Services
     {
         Task<bool> IsUserNameAvailableAsync(string userName);
         Task<bool> IsEmailAvailableAsync(string? email);
+        Task<UserRoles?> IsAccountExistingAsync(UserViewModel account, RolesEnum roleName);
+        bool IsPasswordCorrect(UserRoles userRoles, string rawPass);
     }
 
 
     public class UserAccountServices: IUserAccountServices
     {
         private readonly ApplicationDbContext _context;
-        public UserAccountServices(ApplicationDbContext context)
+        private readonly IPassHasherService _passHasher;
+        public UserAccountServices(ApplicationDbContext context, IPassHasherService passHasher)
         {
             _context = context;
+            _passHasher = passHasher;
         }
+
 
         public async Task<bool> IsUserNameAvailableAsync(string userName)
         {
@@ -30,6 +37,30 @@ namespace Golestan_Simulation.Services
             var isAvailable = await _context.Users.AnyAsync(u => u.Email == email);
 
             return isAvailable;
+        }
+        public async Task<UserRoles?> IsAccountExistingAsync(UserViewModel account, RolesEnum roleName)
+        {
+            try
+            {
+                var ur = await _context.UserRoles.Include(s => s.User).Include(s => s.Role)
+                    .Where(ur => ur.Role.Name == roleName)
+                    .SingleAsync(ur => ur.User.UserName == account.UsernameOrEmail || ur.User.Email == account.UsernameOrEmail);
+                
+                return ur;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+            
+        }
+        public bool IsPasswordCorrect(UserRoles ur, string rawPass)
+        {
+            var hashedPass = _passHasher.HashPassword(rawPass);
+
+            var isCorrect = ur.User.HashedPassword.Equals(hashedPass);
+
+            return isCorrect;
         }
     }
 }
