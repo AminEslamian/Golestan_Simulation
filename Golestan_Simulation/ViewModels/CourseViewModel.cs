fc032847bb068e7b1257a5c9f3f1858903710a5c
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-07-11 12:26:57 +0330

    feat: add CourseViewModel

---
+++ Golestan_Simulation/ViewModels/CourseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Golestan_Simulation.ViewModels
+{
+    public class CourseViewModel
+    {
+        [StringLength(50)]
+        public string? Name { get; set; }
+        [StringLength(20)]
+        public string? Code { get; set; }
+        [StringLength(2)]
+        public string? Unit { get; set; } // Unit should probably be from int type; if changed, change in Course.cs too
+        [StringLength(50)]
+        public string? Description { get; set; }
+        public DateOnly? ExamDate { get; set; } 
+    }
+}
