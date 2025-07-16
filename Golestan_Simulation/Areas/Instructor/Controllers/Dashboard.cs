fac88f52f3777bcb9138ea1d7154cd62d6d7bb7a
EhsnAbdllh <sha.m.a.r.8315@gamil.com>
2025-07-16 15:49:32 +0330

    refactor: move content of old Instructor & Student controllers to new Instructor & Student areas

---
+++ Golestan_Simulation/Areas/Instructor/Controllers/Dashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Golestan_Simulation.Areas.Instructor.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
