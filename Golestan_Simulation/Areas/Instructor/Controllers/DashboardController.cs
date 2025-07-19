87f39989c3561d739ac1a1890b63b3ba5e7c7bab
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-07-19 16:25:00 +0330

    feat: add showsections for instructor

--- Golestan_Simulation/Areas/Instructor/Controllers/DashboardController.cs
+++ Golestan_Simulation/Areas/Instructor/Controllers/DashboardController.cs
@@ -1,6 +1,11 @@
 ﻿using Golestan_Simulation.Services;
+using Golestan_Simulation.Data;
+using Golestan_Simulation.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
 
 namespace Golestan_Simulation.Areas.Instructor.Controllers
 {
@@ -8,8 +13,34 @@ namespace Golestan_Simulation.Areas.Instructor.Controllers
     [Authorize(Roles = "Instructor")]
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public async Task<IActionResult> ShowSections()
+        {
+            var instructorId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var sections = await _context.Sections
+                .Where(s => s.Teachs.Any(t => t.InstructorId.ToString() == instructorId))
+                .Select(s => new
+                {
+                    Id = s.Id,
+                    CourseName = s.Course.Name,
+                    Semester = s.Semester,
+                    Year = s.Year,
+                    ClassroomName = s.Classroom.Id + " - " + s.Classroom.Building + " - " + s.Classroom.RoomNumber,
+                })
+                .ToListAsync();
+
+            ViewBag.Sections = sections;
             return View();
         }
     }
