aa0b5430320fb2f65177b15a3b74831a7cc2d345
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-07-21 00:55:03 +0330

    docs: add commented approach for edit actions

--- Golestan_Simulation/Areas/Admin/Controllers/ClassroomsManagementController.cs
+++ Golestan_Simulation/Areas/Admin/Controllers/ClassroomsManagementController.cs
@@ -79,5 +79,51 @@ namespace Golestan_Simulation.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var classroom = await _context.Classrooms
+        //        .FirstOrDefaultAsync(s => s.Id == id);
+
+        //    if (classroom == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var vm = new ClassroomViewModel
+        //    {
+        //        Building = classroom.Building,
+        //        Capacity = classroom.Capacity,
+        //        RoomNumber = classroom.RoomNumber
+        //    };
+        //    return View(vm);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(ClassroomViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model); // Return same view with validation errors
+        //    }
+
+        //    var product = await _context.Classrooms.FindAsync(model);
+
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    // Update properties
+        //    product.Name = model.Name;
+        //    product.Price = model.Price;
+
+        //    _context.Update(product);
+        //    await _context.SaveChangesAsync();
+
+        //    return RedirectToAction(nameof(Index)); // Redirect to list page or details page
+        //}
+
     }
 }
