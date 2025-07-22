8a83f0ff94d601f7ce206ef79e1541d40117ce00
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-07-22 18:43:32 +0330

    chore: some routing links and test an apporach for claim id

--- Golestan_Simulation/Areas/Instructor/Controllers/SectionsManagementController.cs
+++ Golestan_Simulation/Areas/Instructor/Controllers/SectionsManagementController.cs
@@ -20,7 +20,7 @@ namespace Golestan_Simulation.Areas.Instructor.Controllers
         //this is actually the index!:
         public async Task<IActionResult> Showsections() // Shows students of the selected section
         {
-            var instructorId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var instructorId = User.FindFirstValue("UserId");
 
             var sections = await _context.Sections
                 .Where(s => s.Teachs.Any(t => t.InstructorId.ToString() == instructorId))
@@ -36,6 +36,8 @@ namespace Golestan_Simulation.Areas.Instructor.Controllers
 
             return View(sections);
         }
+
+
 
 
     }
