a94ebe5f278c544603ba3e444e8c8aa8ef55c4ed
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-07-22 23:27:26 +0330

    feat: add the service and use it

---
+++ Golestan_Simulation/Services/CreateClassroomService.cs
@@ -0,0 +1,28 @@
+﻿using Golestan_Simulation.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Golestan_Simulation.Services
+{
+    public interface ICreateClassroomService
+    {
+        Task<bool> IsClassroomAvailableAsync(string? Building, int RoomNumber);
+    }
+
+    public class CreateClassroomService : ICreateClassroomService
+    {
+        private readonly ApplicationDbContext _context;
+        public CreateClassroomService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> IsClassroomAvailableAsync(string? Building, int RoomNumber)
+        {
+            bool exists = await _context.Classrooms
+        .AnyAsync(c => c.Building == Building
+                    && c.RoomNumber == RoomNumber);
+
+            return exists;
+        }
+    }
+}
